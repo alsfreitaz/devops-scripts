e0d8d1d158cf1974252b97cb86be400318f2e3f0
Anderson Freitas <31149794+alsfreitaz@users.noreply.github.com>
2019-09-19 22:54:23 -0300

    Fixed wrong variable name

--- ansible/setup/install_ansible.sh
+++ ansible/setup/install_ansible.sh
@@ -4,7 +4,7 @@ source ../../bash/functions.sh
 source_vars
 
 pyenv install $PYTHON_VERSION -s
-pyenv virtualenv $PYTHON_VERSION $VIRTUALENV_NAME
+pyenv virtualenv $PYTHON_VERSION $ANSIBLE_VIRTUALENV_NAME
 pyenv activate $ANSIBLE_VIRTUALENV_NAME
 pip install --upgrade pip
 pip install -r $PIP_REQUIREMENTS_PATH
