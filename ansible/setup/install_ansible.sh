ad93b08bf35566d24c47f4f7c44bc8a7cda308a9
Anderson Freitas <31149794+alsfreitaz@users.noreply.github.com>
2019-09-19 22:40:22 -0300

    Changed env vars file from ~/.bash_profile to ~/.bashrc

--- ansible/setup/install_ansible.sh
+++ ansible/setup/install_ansible.sh
@@ -5,6 +5,6 @@ source_vars
 
 pyenv install $PYTHON_VERSION -s
 pyenv virtualenv $PYTHON_VERSION $VIRTUALENV_NAME
-pyenv activate $VIRTUALENV_NAME
+pyenv activate $ANSIBLE_VIRTUALENV_NAME
 pip install --upgrade pip
 pip install -r $PIP_REQUIREMENTS_PATH
--- ansible/vars.sh
+++ ansible/vars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export PYTHON_VERSION=3.7.3
-export VIRTUALENV_NAME=ansible
+export ANSIBLE_VIRTUALENV_NAME=ansible_2.8
 export PIP_REQUIREMENTS_PATH="$PWD/setup/pip_requirements.txt"
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
