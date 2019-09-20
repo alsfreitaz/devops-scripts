68e6a37da4bddb3e9026d20dd0bb821c011284fa
Anderson Freitas <31149794+alsfreitaz@users.noreply.github.com>
2019-09-19 22:22:02 -0300

    Changed env vars file from ~/.bash_profile to ~/.bashrc

--- python/setup/install_pyenv__pyenv-virtualenv.sh
+++ python/setup/install_pyenv__pyenv-virtualenv.sh
@@ -8,11 +8,11 @@ else
   curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 fi
 
-grep -qxF 'export PATH="$HOME/.pyenv/bin:$PATH"' $HOME/.bash_profile || echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bash_profile
-grep -qxF 'eval "$(pyenv init -)"' $HOME/.bash_profile || echo 'eval "$(pyenv init -)"' >> $HOME/.bash_profile
-grep -qxF 'eval "$(pyenv virtualenv-init -)"' $HOME/.bash_profile || echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bash_profile
+grep -qxF 'export PATH="$HOME/.pyenv/bin:$PATH"' $HOME/.bashrc || echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bashrc
+grep -qxF 'eval "$(pyenv init -)"' $HOME/.bashrc || echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+grep -qxF 'eval "$(pyenv virtualenv-init -)"' $HOME/.bashrc || echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
 
-source $HOME/.bash_profile
+source $HOME/.bashrc
 
 if [ -d "$(pyenv root)/plugins/pyenv-virtualenv" ]; then
   cd "$(pyenv root)/plugins/pyenv-virtualenv"
